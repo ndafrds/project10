792e23a30fd661734453f61507cb38d1fabfd976
FARID 1972 <FARID 1972@ndafrds>
2018-01-17 11:45:05 +0800

    Project10v2

---
+++ src/test2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FARID 1972
+ */
+import java.util.Scanner;
+public class test2 {
+public static void main(String args[]) { 
+    int x = 30;
+    
+    switch (x) {
+        case 10:
+            System.out.print("Value of x is 10");
+            break;
+        case 20:
+            System.out.print("Value of x is 20");
+            break;
+        case 30:
+            System.out.print("Value of x is 30");
+            break;
+        default:
+            System.out.print("This is else statement");
+            break;
+    }
+  }
+}
+    
+        
+    
+   
+    
+    
